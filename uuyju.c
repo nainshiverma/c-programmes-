98550f4a91c0cd7dc64d8b45aef0729233804d3a
nainshiverma <122629057+nainshiverma@users.noreply.github.com>
2023-01-14 00:43:59 +0530

    Add files via upload

---
+++ uuyju.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int i,j,n;
+	printf("enter n no of rows and columns");
+	scanf("%d",n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n;j++){
+			if(i==j)
+			printf("*");
+			else
+			printf("-");
+		}
+		printf("\n");
+	}
+	return 0;
+}
