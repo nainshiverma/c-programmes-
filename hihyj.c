217a19d02f5b457e016476f011a831c27d7a84e7
nainshiverma <122629057+nainshiverma@users.noreply.github.com>
2023-01-14 00:49:47 +0530

    Add files via upload

---
+++ hihyj.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int i,j,n;
+	printf("enter no of rows");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++){
+			if(i==j)
+			printf("*");
+			else
+			printf("-");
+		}
+		printf("\n");
+    }
+	 for(i=n;i>=1;i--)
+	{
+		for(j=1;j<=n;j++){
+			if(i==j)
+			printf("*");
+			else
+			printf("-");
+		}
+	    printf("\n");
+    }
+    return 0;
+}
