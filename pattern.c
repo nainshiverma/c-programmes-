9f88fa8d6f60df9a73aad80d90e555756842e6eb
nainshiverma <122629057+nainshiverma@users.noreply.github.com>
2023-01-13 23:02:21 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int i,j;
+	for(i=4;i>=2;i-2){
+		for(j=1;j<=4;j++)
+		{
+		if(j<=i)	
+		printf("*");
+		else
+	    printf(" ");
+			
+		}
+	printf("\n");		
+	
+	   
+			
+		
+		
+	}
+	return 0;
+}	
